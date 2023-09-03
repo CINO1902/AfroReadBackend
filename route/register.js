63b78c37e529d4729955ef30d5b85d8f099832b4
CINO1902 <caleboruta.co@gmail.com>
2023-09-03 14:18:43 +0100

    working account

--- route/register.js
+++ route/register.js
@@ -2,39 +2,45 @@ const express = require("express");
 const router = express.Router();
 const register = require('../model/registermodel')
 const {createTokens, validateToken} = require('../jwt/middleware')
-router.route('/createunregistered').post(async (req,res)=>{
-   const {name, email} = req.body;
+router.route('/createaccount').post(async (req,res)=>{
+  const {fullname, email, password, date_of_birth, Security_Question, Security_ans} = req.body;
 
-    let date = new Date();
- 
-    try{
-        let getid =  await register.find({email: email});
-        if(getid.length != 0){
-     
-          return res.json({status:"fail", msg:"User Already Exist"})
-        }else{
-            const accessToken = createTokens(email)
-          await register.create({
-              name: name,
-              email: email, 
-              token:accessToken,
-              date:date    
-          }).then( async()=>{
-           
-            return  res.json({status:"success", msg:"Dashboard Created", token:accessToken})
-          }).catch((err)=>{
-              if(err){
-                console.error(err)
-                return  res.json({status:"fail", msg:"something went wrong"})
-              }
-          })
-         
-
-        }
-      }catch(err){
-        console.error(err)
-         return res.json({status:"fail", msg:"something went wrong"})
-      }
+   let date = new Date();
+   let date_of_birth_day = date_of_birth.split('/')[0]
+   let date_of_birth_month = date_of_birth.split('/')[1]
+   let date_of_birth_year = date_of_birth.split('/')[2]
+   try{
+       let getid =  await register.find({email: email});
+       if(getid.length != 0){
+    
+         return res.json({status:"fail", msg:"User Already Exist"})
+       }else{
+           const accessToken = createTokens(email)
+        let hashpassword = await bcrypt.hash(password,10)
+         await register.create({
+           fullname: fullname,
+             email: email, 
+             password:hashpassword,
+             token:accessToken,
+             date_of_birth_day:date_of_birth_day,
+             date_of_birth_month:date_of_birth_month,
+             date_of_birth_year:date_of_birth_year,
+             Security_question:Security_Question,
+             Security_answer:Security_ans,
+             date:date    
+         }).then( async()=>{ 
+           return  res.json({status:"success", msg:"Dashboard Created", token:accessToken})
+         }).catch((err)=>{
+             if(err){
+               console.error(err)
+               return  res.json({status:"fail", msg:"something went wrong"})
+             }
+         })
+       }
+     }catch(err){
+       console.error(err)
+        return res.json({status:"fail", msg:"something went wrong"})
+     }
 })
 
 module.exports = router
